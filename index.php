71ac1161774da6001f37ba998fb128d9f0091f1b
Zarwine <ange_550@hotmail.com>
2020-02-24 11:06:46 +0100

    first commit

---
+++ index.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="public/css/style.css">
+    <title>Jean Forteroche</title>
+    <meta name="description" content="Blog de Jean Forteroche">
+    <link rel="shortcut icon" type="image/x-icon" href="public\img\book-open-solid.svg">
+</head>
+
+<body>
+    <div id="header"></div>
+    <div id="content"></div>
+    <div id="footer"></div>
+</body>
+
+</html>
+
+<!-- Script pour initialiser le module de traitement de texte de Wordpress
+<head>   
+    <script src="https://cdn.tiny.cloud/1/rhmcwo4c3c04oqicyi140d661xaxcuor848zntmj4er65w6b/tinymce/5/tinymce.min.js"
+    referrerpolicy="origin"></script>
+
+    <script>
+        tinymce.init({
+            selector: '#mytextarea'
+        });
+    </script>
+</head>
+
+<body>
+    <h1>
+        TinyMCE Quick Start Guide
+    </h1>
+    <form method="post">
+        <textarea id="mytextarea" name="mytextarea">
+            Hello, World!
+        </textarea>
+    </form>
+</body>
+
+API key:
+rhmcwo4c3c04oqicyi140d661xaxcuor848zntmj4er65w6b -->
